4632c6819c080564d02cd10cbce4327da97b8b9f
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2023-09-17 01:37:02 +0000

    Finished game hands functionality

--- app.js
+++ app.js
@@ -31,43 +31,39 @@ function computerSelection() {
       return "Error";
   }
 }
+
+let targetScore = 5;
 let userScore = 0;
 let computerScore = 0;
-let stat;
+let userChoice;
+let computerChoice;
 let info;
 
 const checkWinner = function (userChoice, computerChoice) {
   if (userChoice === computerChoice) {
-    stat = 2;
     info = "It's a tie";
     return info;
   } else if (userChoice === "rock" && computerChoice === "paper") {
-    stat = 0;
     computerScore++;
     info = "Rock lost to Paper";
     return info;
   } else if (userChoice === "rock" && computerChoice === "scissors") {
-    stat = 1;
     userScore++;
     info = "You won, Rock beats Scissors";
     return info;
   } else if (userChoice === "paper" && computerChoice === "rock") {
-    stat = 1;
     userScore++;
     info = "You won, Paper beats Rock";
     return info;
   } else if (userChoice === "paper" && computerChoice === "scissors") {
-    stat = 0;
     computerScore++;
     info = "Paper lost to Scissors";
     return info;
   } else if (userChoice === "scissors" && computerChoice === "rock") {
-    stat = 0;
     computerScore++;
     info = "Scissors lost to Rock";
     return info;
   } else if (userChoice === "scissors" && computerChoice === "paper") {
-    stat = 1;
     userScore++;
     info = "You won, Scissors beats paper";
     return info;
@@ -77,12 +73,11 @@ const checkWinner = function (userChoice, computerChoice) {
 };
 
 const playGame = function () {
-  let targetScore = 5;
   while (userScore !== targetScore && computerScore !== targetScore) {
-    let = userChoice = prompt(
-      "Choose between rock, paper, and scissors"
-    ).toLowerCase();
-    let computerChoice = computerSelection();
+    // let userChoice = prompt(
+    //   "Choose between rock, paper, and scissors"
+    // ).toLowerCase();
+    computerChoice = computerSelection();
     checkWinner(userChoice, computerChoice);
     console.log(info);
     console.log(`Your Score: ${userScore} - Computer Score: ${computerScore}`);
@@ -101,22 +96,21 @@ let scoreContainer = document.createElement("div");
 let imageContainer = document.createElement("div");
 let buttonDiv = document.createElement("div");
 let btn = document.createElement("button");
-let btn1 = document.createElement("button");
-let btn2 = document.createElement("button");
-let btn3 = document.createElement("button");
+let rockBtn = document.createElement("button");
+let paperBtn = document.createElement("button");
+let scissorsBtn = document.createElement("button");
 let testBtn = document.createElement("button"); //! Delete Later
 let greetings = document.createElement("p");
 let headerMessage = document.createElement("p");
 let winMessage = document.createElement("p");
 let decideMessage = document.createElement("p");
-let computerChoice = document.createElement("p");
+let computerScoreChoice = document.createElement("p");
 let playerChoice = document.createElement("p");
-let rockLeft = document.createElement("img");
-let rockRight = document.createElement("img");
-let paperLeft = document.createElement("img");
-let paperRight = document.createElement("img");
-let scissorsLeft = document.createElement("img");
-let scissorsRight = document.createElement("img");
+let computerHand = document.createElement("img");
+let playerHand = document.createElement("img");
+
+/*------------------------------------------------*/
+/*------------------Greeting Page-----------------*/
 
 const firstUI = () => {
   btn.textContent = "Play";
@@ -129,39 +123,104 @@ const firstUI = () => {
   changeUI("secondUI");
 };
 
+/*-------------------------------------------------*/
+/*--------------------Game Page--------------------*/
+
 const secondUI = () => {
+  // Heading
   headerMessage.textContent = "Rock Paper Scisors By SelormDev";
   headerMessage.classList.add("header-message");
-  btn1.textContent = "Rock";
-  btn2.textContent = "Paper";
-  btn3.textContent = "Scisssors";
 
+  // Playing Buttons (Rock, Paper, Scissors)
+  rockBtn.textContent = "Rock";
+  paperBtn.textContent = "Paper";
+  scissorsBtn.textContent = "Scisssors";
+  // Test Button
   testBtn.textContent = "Return";
   testBtn.classList.add("btn", "btn-test");
-
-  computerChoice.textContent = "Computer Score: ";
-  playerChoice.textContent = "Player Score: ";
-  computerChoice.classList.add("score-text");
+  // Score Kepping message
+  computerScoreChoice.textContent = `Computer Score: ${computerScore}`;
+  playerChoice.textContent = `Player Score: ${userScore}`;
+  computerScoreChoice.classList.add("score-text");
   playerChoice.classList.add("score-text");
-  scoreContainer.append(playerChoice, computerChoice);
+  scoreContainer.append(computerScoreChoice, playerChoice);
   scoreContainer.classList.add("score-container");
 
-  rockLeft.src = "./img/rock-left-hand.png";
-  rockLeft.classList.add("transform", "invert-color");
+  computerHand.src = "./img/rock1.png";
+  computerHand.classList.add("transform");
+  playerHand.src = "./img/rock.png";
 
-  paperRight.src = "./img/Paper-left-hand.png";
-  paperRight.classList.add("transform");
-
-  imageContainer.append(rockLeft, paperRight);
+  imageContainer.append(computerHand, playerHand);
   imageContainer.classList.add("image-container");
 
   decideMessage.textContent = "Choose your move, Rock, Paper or Scissors?";
   decideMessage.classList.add("decide-text");
+
   buttonDiv.classList.add("btnParent");
-  btn1.classList.add("btn", "btn-game");
-  btn2.classList.add("btn", "btn-game");
-  btn3.classList.add("btn", "btn-game");
-  buttonDiv.append(btn1, btn2, btn3);
+  rockBtn.classList.add("btn", "btn-game");
+  paperBtn.classList.add("btn", "btn-game");
+  scissorsBtn.classList.add("btn", "btn-game");
+  buttonDiv.append(rockBtn, paperBtn, scissorsBtn);
+
+  testBtn.addEventListener("click", firstUI);
+
+  rockBtn.addEventListener("click", function () {
+    userChoice = "rock";
+    computerChoice = computerSelection();
+    playerHand.src = "./img/rock.png";
+    playerHand.classList.remove("transform", "invert-color");
+    if (computerChoice === "rock") {
+      computerHand.src = "./img/rock1.png";
+      computerHand.classList.add("transform");
+      computerHand.classList.remove("invert-color");
+    } else if (computerChoice === "paper") {
+      computerHand.src = "./img/paper1.png";
+      computerHand.classList.remove("transform", "invert-color");
+    } else {
+      computerHand.src = "./img/scissors1.png";
+      computerHand.classList.add("transform", "invert-color");
+    }
+  });
+
+  paperBtn.addEventListener("click", function () {
+    userChoice = "paper";
+    computerChoice = computerSelection();
+    playerHand.src = "./img/paper.png";
+    playerHand.classList.add("transform");
+    playerHand.classList.remove("invert-color");
+    if (computerChoice === "rock") {
+      computerHand.src = "./img/rock1.png";
+      computerHand.classList.add("transform");
+      computerHand.classList.remove("invert-color");
+    } else if (computerChoice === "paper") {
+      computerHand.src = "./img/paper1.png";
+      computerHand.classList.remove("transform", "invert-color");
+    } else {
+      computerHand.src = "./img/scissors1.png";
+      computerHand.classList.add("transform", "invert-color");
+    }
+    console.log(checkWinner(userChoice, computerChoice));
+  });
+
+  scissorsBtn.addEventListener("click", function () {
+    userChoice = "scissors";
+    computerChoice = computerSelection();
+    playerHand.src = "./img/scissors.png";
+    playerHand.classList.add("invert-color");
+    playerHand.classList.remove("transform");
+
+    if (computerChoice === "rock") {
+      computerHand.src = "./img/rock1.png";
+      computerHand.classList.add("transform");
+      computerHand.classList.remove("invert-color");
+    } else if (computerChoice === "paper") {
+      computerHand.src = "./img/paper1.png";
+      computerHand.classList.remove("transform", "invert-color");
+    } else {
+      computerHand.src = "./img/scissors1.png";
+      computerHand.classList.add("transform", "invert-color");
+    }
+  });
 
   container.append(
     headerMessage,
@@ -173,7 +232,6 @@ const secondUI = () => {
     buttonDiv
   );
 
-  testBtn.addEventListener("click", firstUI);
   changeUI("firstUI");
 };
 
